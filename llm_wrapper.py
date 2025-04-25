fcc3139d73515cb9ee9861efddcc3a8d2ddbe6a0
Roman Senior <gitmy@outlook.com>
2025-04-25 10:39:26 +0300

    detect language

--- llm_wrapper.py
+++ llm_wrapper.py
@@ -1,22 +1,22 @@
 
 from openai import OpenAI
 import traceback
-from prompt import SYSTEM_PROMPT
+from prompt import SYSTEM_PROMPT, LANGUAGE_PROMPT
 class LLM_Wrapper:
     def __init__(self, api_key:str):
-        self.model_name = "gpt-4o-mini"
+        self.model_name = "gpt-4.1-mini"
         self.client = OpenAI(api_key=api_key)
 
     def generate_response(self, user_post: str) -> str:
         print(f"Post: {user_post}")
         # Placeholder for generating a response
-        messages = [{"role": "system", "content": SYSTEM_PROMPT}]
-        messages.append({"role": "user", "content": user_post})
+        language = self.detect_language(user_post)
+        print(f"Language: {language}")
         result_text = None
         try:
             response = self.client.responses.create(
                 model=self.model_name,
-                instructions=SYSTEM_PROMPT,
+                instructions=SYSTEM_PROMPT.format(language),
                 input=user_post,
             )
             # completion = openai.ChatCompletion.create(
@@ -29,3 +29,16 @@ class LLM_Wrapper:
             traceback.print_exc()
         print(f"Response: {result_text}")
         return result_text
+    
+    def detect_language(self, text: str) -> str:
+        # Placeholder for language detection
+        try:
+            response = self.client.responses.create(
+                model=self.model_name,
+                instructions=LANGUAGE_PROMPT,
+                input=text,
+            )
+            return response.output_text
+        except:
+            traceback.print_exc()
+        return "English"
